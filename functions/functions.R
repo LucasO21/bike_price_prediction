e9b28a6bf12e5cb1d0e2877da4279d1a0fcb2dbb
clfo2014@gmail.com <clfo2014@gmail.com>
2022-10-14 18:51:26 -0400

    #01 plot bike prediction complete

--- functions/functions.R
+++ functions/functions.R
@@ -14,17 +14,26 @@ library(tidyverse)
 library(janitor)
 library(tidymodels)
 
+
 # ******************************************************************************
 # LOAD ARTIFACTS ----
 # ******************************************************************************
 
 model_artifacts_list <- read_rds("../artifacts/model_artifacts.rds")
 
-data_tbl <- read_rds("../data/trekbikes_clead_data.rds") 
+data_tbl <- read_rds("../data/trekbikes_clead_data.rds") %>% 
+    filter(frame_material %in% c("Carbon", "Aluminum"))
 
 data_tbl %>% View()
 data_tbl %>% count(frame_material)
 
+bike_model <- "Dual Sport+ 2 Stagger"
+bike_year  <- 2023
+.ml_model  <- model_artifacts_list[[1]]
+
+# ******************************************************************************
+# FUNCTIONS ----
+# ******************************************************************************
 
 # PRICE PREDICTION FUNCTION ----
 get_new_bike_price <- function(bike_model, bike_year, .ml_model){
@@ -60,7 +69,7 @@ get_new_bike_price <- function(bike_model, bike_year, .ml_model){
     
 }
 
-get_new_bike_price("Fuel EX 5 Gen 5", 2023, "RANDOM FOREST") %>% glimpse()
+get_new_bike_price(bike_model, 2023, "RANDOM FOREST")
 
 
 # PRICE PREDICTION TABLE ----
@@ -83,8 +92,9 @@ get_new_bike_price_table <- function(data){
     return(data_prep)
 }
 
-get_new_bike_price("Fuel EX 5 Gen 5", 2023, "RANDOM FOREST") %>% 
+get_new_bike_price(bike_model, 2023, "RANDOM FOREST") %>% 
     get_new_bike_price_table()
+
 
 
 # PREP DATA FOR PREDICTION PLOT ----
@@ -103,3 +113,36 @@ get_price_prediction_data <- function(data_tbl, new_bike_tbl){
 }
 
 get_price_prediction_data(data_tbl, new_bike_tbl) %>% tail()
+
+
+# PREDICTION PLOT ----
+get_price_prediction_plot <- function(data){
+    
+    p <- data %>% 
+        mutate(family = fct_reorder(family, model_price)) %>% 
+        mutate(frame_material = frame_material %>% fct_rev()) %>% 
+        mutate(label_text = str_glue("Unit Price: {scales::dollar(model_price, accuracy = 1)}
+                                     Model: {model_name}
+                                     Category: {category}
+                                     Family: {family}
+                                     Frame Material: {frame_material}")) %>% 
+        ggplot(aes(family, model_price, color = estimate))+
+        geom_violin(scale = "width")+
+        geom_jitter(aes(text = label_text), width = 0.1, alpha = 0.6, size = 2)+
+        facet_wrap(~ frame_material)+
+        coord_flip()+
+        scale_y_log10(labels = scales::dollar_format(accuracy = 1))+
+        tidyquant::scale_color_tq()+
+        tidyquant::theme_tq()+
+        theme(strip.text.x = element_text(margin = margin(6, 6, 6, 6)))+
+        theme(strip.text.x = element_text(size = 12, face = "bold"))+
+        labs(title = NULL, y = NULL, y = "Log Scale")
+    
+    plotly::ggplotly(p, tooltip = "text")
+    
+    
+    
+}
+
+get_price_prediction_data(data_tbl, new_bike_tbl) %>%
+    get_price_prediction_plot()
